005a7df77b23ef42ba90a50845ad21a83fe63fc1
Your Name <you@example.com>
2026-05-29 01:03:01 +0200

    More work on javascript

--- Javascript/quiz.js
+++ Javascript/quiz.js
@@ -1,19 +1,36 @@
-//Variabler
-let svar = null;
-const forbiddenCharacters = "!@#$%^&*()_+=[]{}|;:'\",<>/?`~\\";
-const form = document.getElementById("inloggForm");
+/* Skapar variablar som länkar till HTML-elementen */
+const answer1 = document.getElementById("answer1");
+const answer2 = document.getElementById("answer2");
+const answer3 = document.getElementById("answer3");
 
-function valideraSvar(svar) {
-  //Sätter en gräns för antalet tecken svaret behöver innehålla samt ogiltighetsförklarar specialtecken.
-  if (svar.length < 2) {
-    return false; //För kort.
+const submit = document.getElementById("submit");
+
+const result1 = document.getElementById("result1");
+const result2 = document.getElementById("result2");
+const result3 = document.getElementById("result3");
+
+/*Variablerna som krävs */
+const correctAnswer1 = "Bosilkovska";
+const correctAnswer2 = "2026-06-04";
+const correctAnswer3 = "Harvey Metcalfe";
+
+function checkAnswer() {
+  /*Kollar om svaret är rätt eller fel och skriver ut resultatet i HTML-elementen*/
+  if (answer1.value === correctAnswer1) {
+    result1.textContent = "Correct!";
+  } else {
+    result1.textContent = "Wrong!";
   }
 
-  for (let char of svar) {
-    if (forbiddenCharacters.includes(char)) {
-      return false; //Ogiltigt tecken.
-    }
+  if (answer2.value === correctAnswer2) {
+    result2.textContent = "Correct!";
+  } else {
+    result2.textContent = "Wrong!";
   }
 
-  return true; //Godkänt svar.
+  if (answer3.value === correctAnswer3) {
+    result3.textContent = "Correct!";
+  } else {
+    result3.textContent = "Wrong!";
+  }
 }
